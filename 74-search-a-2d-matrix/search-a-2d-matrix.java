23a4c5c4367693a2582d480dd2293930d11b43a1
Piyush Rathod <160535498+PiyushPRathod@users.noreply.github.com>
2025-09-05 11:47:17 -0400

    Time: 0 ms (100.00%) | Memory: 42.1 MB (66.05%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,21 +1,56 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) 
     {
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        for(int i = 0; i < row; i++)
+        //Binary Search method
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
         {
-            for(int j = 0; j < col; j++)
-            {
-                if(matrix[i][j] == target)
-                {
-                    return true;
-                }
-            }
+            return false;
         }
 
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int left = 0;
+        int right = row * col - 1;
+
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            int midVal = matrix[mid / col][mid % col];
+
+            if(midVal == target)
+            {
+                return true;
+            }
+            else if(midVal < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+
+        }
         return false;
+
+
+        // //Basic searching method
+        // int row = matrix.length;
+        // int col = matrix[0].length;
+
+        // for(int i = 0; i < row; i++)
+        // {
+        //     for(int j = 0; j < col; j++)
+        //     {
+        //         if(matrix[i][j] == target)
+        //         {
+        //             return true;
+        //         }
+        //     }
+        // }
+
+        // return false;
         
     }
 }
