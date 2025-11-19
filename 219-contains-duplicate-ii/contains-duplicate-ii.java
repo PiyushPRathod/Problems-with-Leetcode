d0f562dd7ee2f3bfb5039873371a2dc70a238adf
Piyush Rathod <160535498+PiyushPRathod@users.noreply.github.com>
2025-11-19 12:33:16 -0500

    Time: 25 ms (27.13%) | Memory: 76.5 MB (17.72%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,21 +1,43 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) 
     {
-         int n = nums.length;
+        // Optimal Solution
+         HashSet<Integer> set = new HashSet<>();
 
-        for (int i = 0; i < n; i++) 
-        {
-            // only check j within k distance
-            for (int j = i + 1; j <= i + k && j < n; j++) 
-            {
-                if (nums[i] == nums[j]) 
-                {
-                    return true;
-                }
+        for (int i = 0; i < nums.length; i++) {
+
+            // If value already exists in window → valid duplicate
+            if (set.contains(nums[i])) {
+                return true;
+            }
+
+            set.add(nums[i]);
+
+            // Maintain window size <= k
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
             }
         }
 
         return false;
+
+
+        // // Brute force solution
+        // int n = nums.length;
+
+        // for (int i = 0; i < n; i++) 
+        // {
+        //     // only check j within k distance
+        //     for (int j = i + 1; j <= i + k && j < n; j++) 
+        //     {
+        //         if (nums[i] == nums[j]) 
+        //         {
+        //             return true;
+        //         }
+        //     }
+        // }
+
+        // return false;
         
     }
 }
