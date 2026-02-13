94b02db494abdba7ac69c473d4d46ede988b3cea
Piyush Rathod <160535498+PiyushPRathod@users.noreply.github.com>
2026-02-13 08:31:22 -0500

    Time: 2 ms (99.17%) | Memory: 47.5 MB (9.07%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,32 +1,39 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) 
     {
-        // // Brute Force Solution
+        // Optimal Solution
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            int compliment = target - nums[i];
+
+            if(map.containsKey(compliment))
+                return new int[] {map.get(compliment), i};
+            
+            map.put(nums[i], i);
+
+        }
+
+        return new int[] {};
+
+
+        // // Brute force
+        // int a = 0, b = 0;
+
         // for(int i = 0; i < nums.length; i++)
         // {
         //     for(int j = i + 1; j < nums.length; j++)
         //     {
         //         if(nums[i] + nums[j] == target)
         //         {
-        //            return new int[] {i , j};
+        //             a = i;
+        //             b = j;
         //         }
         //     }
         // }
-        // return new int[] {};  
 
-        // HashMap solution
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < nums.length; i++)
-        {
-            int complement = target - nums[i];
-            if(map.containsKey(complement))
-            {
-                return new int[] {map.get(complement), i};
-            }
-            map.put(nums[i], i);
-        } 
-
-        return new int[] {};
+        // return new int[]{a, b};
+        
     }
 }
