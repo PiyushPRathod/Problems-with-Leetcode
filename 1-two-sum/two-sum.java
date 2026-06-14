1e616729e90362a77226c3654cbd242c315ee301
Piyush Rathod <160535498+PiyushPRathod@users.noreply.github.com>
2026-06-14 18:43:58 -0400

    Time: 2 ms (99.39%) | Memory: 47.1 MB (39.62%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -2,18 +2,35 @@ class Solution
 {
     public int[] twoSum(int[] nums, int target) 
     {
-        // Brute force
+        // Optimal Solution
+        Map<Integer, Integer> map = new HashMap<>();
+
         for(int i = 0; i < nums.length; i++)
         {
-            for(int j = i + 1; j < nums.length; j++)
+            int compliment = target - nums[i];
+            if(map.containsKey(compliment))
             {
-                if(nums[i] + nums[j] == target)
-                {
-                    return new int[] {i, j};
-                }
+                return new int[] {map.get(compliment), i};
             }
+
+            map.put(nums[i], i);
         }
 
         return new int[] {};
+
+
+        // // Brute force
+        // for(int i = 0; i < nums.length; i++)
+        // {
+        //     for(int j = i + 1; j < nums.length; j++)
+        //     {
+        //         if(nums[i] + nums[j] == target)
+        //         {
+        //             return new int[] {i, j};
+        //         }
+        //     }
+        // }
+
+        // return new int[] {};
     }
 }
